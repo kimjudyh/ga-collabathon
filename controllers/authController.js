62cd69d726c1e93a3edeba747006ef6fcf5600b0
lisa-liang <lianglisa1@gmail.com>
2020-05-21 15:07:38 -0700

    added sign up routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,44 @@ const router = express.Router();
 const db = require('../models');
 
 // ==== ROUTES
+// GET Register New
+router.get('/', (req, res) => {
+    res.render('/signup', {
+        title: 'Sign up',
+    });
+});
+
+
+// POST Register Create (User)
+router.post('/', async (req, res) => {
+    // Validating
+    console.log('New User Obj = ', req.body);
+    try {
+      // Create A New User
+      // Redirect To Home Page
+      const user = await db.User.findOne({username: req.body.username});
+  
+      // Check If We Got A User Object Back From The Database
+      if (user) {
+        return res.send('<h1>Account already exists, please login</h1>');
+      }
+  
+      // TODO: Hash Password
+  
+      const userData = {
+        username: req.body.username,
+        email: req.body.email,
+      }
+  
+      // Creating the new user
+      await db.User.create(userData);
+  
+      // Redirect to home page
+      res.redirect('/');
+    } catch (err) {
+      res.send(err);
+    }
+  });
 
 // ==== EXPORTS
 module.exports = router;
